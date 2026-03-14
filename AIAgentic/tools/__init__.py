9a0fc363fe9ba0daff7aab0162ff29e6e0c12acc
Róbert Malovec <robert@malovec.sk>
2026-03-14 11:26:34 +0000

    Add tools/__init__.py, CHANGES.txt, MANIFEST.in, pytest.ini

--- AIAgentic/tools/__init__.py
+++ AIAgentic/tools/__init__.py
@@ -1,17 +1,24 @@
 # Apache License 2.0
-#
 # Copyright (c) 2026 Róbert Malovec
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-"""Tool bridge modules for robotframework-aiagentic."""
+"""
+Strands @tool decorated functions that bridge Robot Framework libraries.
+
+Each tool module wraps a specific RF library's keywords as Strands tools,
+enabling AI agents to interact with applications through the same
+battle-tested RF libraries that manual tests use.
+"""
+
+from .web_tools import WEB_TOOLS
+from .api_tools import API_TOOLS
+from .mobile_tools import MOBILE_TOOLS
+from .common_tools import COMMON_TOOLS
+from .browser_analysis_tools import BROWSER_ANALYSIS_TOOLS
+
+__all__ = [
+    "WEB_TOOLS",
+    "API_TOOLS",
+    "MOBILE_TOOLS",
+    "COMMON_TOOLS",
+    "BROWSER_ANALYSIS_TOOLS",
+]
